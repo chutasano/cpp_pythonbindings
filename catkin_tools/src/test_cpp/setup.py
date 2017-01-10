d8c88c4de0ac70607fd0fae44fb75d5ecc3cc5b6
Chuta Sano <chuta_japan@comcast.net>
2017-01-10 13:56:37 -0500

    works

--- catkin_tools/src/test_cpp/setup.py
+++ catkin_tools/src/test_cpp/setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-  packages=['test_cpp'],
-  package_dir={'': 'src'},
+setup(
+    version= '0.0.0',
+    packages=['test_cpp'],
+    package_dir={'': 'src'}
 )
-
-setup(**setup_args)
+# from catkin_pkg.python_setup import generate_distutils_setup
+# 
+# setup_args = generate_distutils_setup(
+#   packages=['test_cpp'],
+#   package_dir={'': 'src'},
+# )
+# 
+# setup(**setup_args)
